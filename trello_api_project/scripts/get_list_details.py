e0911f2e90f1ec2d831fafb61dcbad69f2c50b08
Kai Indigo Wolf <kai.indigo.wolf@gmail.com>
2023-09-13 15:21:09 -0500

    Remove get_trello_board as I now have board id to use in project, modify get_list_details to create dict mapping of lists and names

---
+++ trello_api_project/scripts/get_list_details.py
@@ -0,0 +1,26 @@
+import requests
+from trello_api_project.config.trello_config import API_KEY, API_TOKEN, BOARD_ID
+
+def get_list_details():
+    url = f"https://api.trello.com/1/boards/{BOARD_ID}/lists"
+
+    headers = {
+        "Accept": "application/json"
+    }
+
+    query = {
+        'key': API_KEY,
+        'token': API_TOKEN
+    }
+
+    response = requests.request(
+        "GET",
+        url,
+        headers=headers,
+        params=query
+    )
+
+    lists_details = response.json()
+    list_id_mapping = {list_detail['name']: list_detail['id'] for list_detail in lists_details}
+    
+    return list_id_mapping
