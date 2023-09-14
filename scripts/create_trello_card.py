a4eff6c4c7dbae14f4cdd376bdf51cf82c6764ad
Kai Indigo Wolf <kai.indigo.wolf@gmail.com>
2023-09-13 19:48:02 -0500

    Final push

--- scripts/create_trello_card.py
+++ scripts/create_trello_card.py
@@ -1,6 +1,7 @@
 import requests
 from config.trello_config import API_KEY, API_TOKEN, BOARD_ID
 
+
 def create_card(list_id, name, desc=None):
     url = "https://api.trello.com/1/cards"
     query = {
@@ -11,14 +12,13 @@ def create_card(list_id, name, desc=None):
         'key': API_KEY,
         'token': API_TOKEN
     }
-    print(f"Creating card with name: {name}, in list: {list_id}")  # Add this line
+    print(f"Creating card with name: {name}, in list: {list_id}")
     try:
         response = requests.post(url, params=query)
         response.raise_for_status()
     except requests.exceptions.RequestException as e:
         print(f"An error occurred while creating a card: {e}")
     else:
-        print(response.json())  # Print the API response
         return response.json()
 
 def create_checklist(card_id, name):
@@ -28,14 +28,13 @@ def create_checklist(card_id, name):
         'key': API_KEY,
         'token': API_TOKEN
     }
-    print(f"Creating checklist with name: {name}, on card: {card_id}")  # Add this line
+    print(f"Creating checklist with name: {name}, on card: {card_id}")
     try:
         response = requests.post(url, params=query)
         response.raise_for_status()
     except requests.exceptions.RequestException as e:
         print(f"An error occurred while creating a checklist: {e}")
     else:
-        print(response.json())  # Print the API response
         return response.json()
 
 def create_checklist_item(checklist_id, name):
@@ -45,12 +44,11 @@ def create_checklist_item(checklist_id, name):
         'key': API_KEY,
         'token': API_TOKEN
     }
-    print(f"Creating checklist item with name: {name}, on checklist: {checklist_id}")  # Add this line
+    print(f"Creating checklist item with name: {name}, on checklist: {checklist_id}") 
     try:
         response = requests.post(url, params=query)
         response.raise_for_status()
     except requests.exceptions.RequestException as e:
         print(f"An error occurred while creating a checklist item: {e}")
     else:
-        print(response.json())  # Print the API response
         return response.json()
